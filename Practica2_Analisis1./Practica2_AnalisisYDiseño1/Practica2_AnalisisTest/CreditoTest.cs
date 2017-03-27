4e1a2eadbdf7a95904e2d2ffd23af9adfba00acc
EduardoCaseros <eduard.caseros@gmail.com>
2017-03-27 10:29:30 -0600

    Update CreditoTest.cs
    Prueba credito modifiada

--- Practica2_Analisis1./Practica2_AnalisisYDiseño1/Practica2_AnalisisTest/CreditoTest.cs
+++ Practica2_Analisis1./Practica2_AnalisisYDiseño1/Practica2_AnalisisTest/CreditoTest.cs
@@ -11,11 +11,11 @@ namespace Practica2_AnalisisyDiseño1.Tests
         public void Credito()
         {
             //Arrange o Planteamiento
-            const int numero_acreditador = 1234; //cuenta de la persona en el sistema
-            const int numero_acreditado = 4321222; //cuenta de la persona a acreditar
-            const float monto = 110;//monto de la acreditacion
+            const int numero_acreditador = 4321; //cuenta de la persona en el sistema
+            const int numero_acreditado = 542; //cuenta de la persona a acreditar
+            const float monto = 50;//monto de la acreditacion
             const String descripcion = "Credito de la cuenta 100 a la 200 monto 500";//descripcion de la acreditacion
-            const float esperado = -1;
+            const float esperado = 1;
 
             //Act o Prueba
             var actual = BaseDatos.Credito(numero_acreditador,numero_acreditado,monto,descripcion);

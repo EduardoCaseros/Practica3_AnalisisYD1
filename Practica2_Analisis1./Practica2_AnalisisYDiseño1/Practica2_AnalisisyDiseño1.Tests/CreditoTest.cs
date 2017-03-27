985f6c4ce7dea32febd9b91b1e7766015a56314c
EduardoCaseros <eduard.caseros@gmail.com>
2017-03-27 11:03:20 -0600

    Update CreditoTest.cs
    Modificada

--- Practica2_Analisis1./Practica2_AnalisisYDiseño1/Practica2_AnalisisyDiseño1.Tests/CreditoTest.cs
+++ Practica2_Analisis1./Practica2_AnalisisYDiseño1/Practica2_AnalisisyDiseño1.Tests/CreditoTest.cs
@@ -11,11 +11,11 @@ namespace Practica2_AnalisisyDiseño1.Tests
         public void Credito()
         {
             //Arrange o Planteamiento
-            const int numero_acreditador = 100; //cuenta de la persona en el sistema
-            const int numero_acreditado = 200; //cuenta de la persona a acreditar
-            const float monto = 500;//monto de la acreditacion
+            const int numero_acreditador = 4321; //cuenta de la persona en el sistema
+            const int numero_acreditado = 542; //cuenta de la persona a acreditar
+            const float monto = 50;//monto de la acreditacion
             const String descripcion = "Credito de la cuenta 100 a la 200 monto 500";//descripcion de la acreditacion
-            const float esperado = 500;
+            const float esperado = 1;
 
             //Act o Prueba
             var actual = BaseDatos.credito(numero_acreditador,numero_acreditado,monto,descripcion);
